1cdc4e46136cbe3eecf2352094e4fa7017ca2922
dani-noumena <155535020+dani-noumena@users.noreply.github.com>
2025-07-15 10:54:13 +0200

    ST-4732 Added homebrew formula for npl-cli install (#1)

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# homebrew-tools
+# Homebrew Tools
+
+This repository contains the formula for the installation of the NPL (Noumena Protocol Language) CLI tool via Homebrew.
+
+## Installation
+
+First, we assume you have Homebrew installed on your system. If you haven't installed it yet, you can do so by following 
+the instructions at [Homebrew's official website](https://brew.sh/).
+
+### NPL CLI Tool
+File `npl.rb` provides the Homebrew formula for the NPL CLI tool, which is used to manage and interact with NPL projects. 
+For more information on the `npl-cli`, refer to its section in our [documentation](https://documentation.noumenadigital.com/runtime/tools/build-tools/cli/)
+or to the [public repository](https://github.com/NoumenaDigital/npl-cli).
+
+Since our formula is self-hosted, you need to tap our repository first. 
+
+Tapping and installation can be done in one step using the following command:
+
+```bash
+brew install NoumenaDigital/tools/npl
+```
---
+++ npl.rb
@@ -0,0 +1,40 @@
+class Npl < Formula
+  desc "CLI for NPL (NOUMENA Protocol Language)"
+  homepage "https://github.com/NoumenaDigital/npl-cli"
+  version "2025.1.9"
+  license "Apache-2.0"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/NoumenaDigital/npl-cli/releases/download/#{version}/npl-cli-macos-aarch64-#{version}"
+      sha256 "fd6d8194300aefcd63f9e590bdba15a49d927f98967d23e9d89aba225354162f"
+    else
+      url "https://github.com/NoumenaDigital/npl-cli/releases/download/#{version}/npl-cli-macos-x86_64-#{version}"
+      sha256 "e49f68c7320082c157f1ea0400bfb3170f28006e483d7001347a3fe939cc27e2"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.arm?
+      url "https://github.com/NoumenaDigital/npl-cli/releases/download/#{version}/npl-cli-linux-aarch64-#{version}"
+      sha256 "d462774d62750cb153f20d0797ca11e14a9cbfa5d2a24ec6894fccaf2fc5ab14"
+    else
+      url "https://github.com/NoumenaDigital/npl-cli/releases/download/#{version}/npl-cli-linux-x86_64-#{version}"
+      sha256 "7e9e848317a9c1fef845f50f356e65c62e57db130812a5e70f62e58274976c46"
+    end
+  end
+
+  def install
+    bin.install "#{stable.url.split('/').last}" => "npl"
+  end
+
+  test do
+    system "#{bin}/npl", "help"
+  end
+
+  livecheck do
+      url :stable
+      regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+end
